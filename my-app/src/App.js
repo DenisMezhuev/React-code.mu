8b38e42bbaac33112e37cfc7a9555b4c5f4dc397
Denis Ermakov <alexmezhuev@yandex.by>
2023-05-28 17:32:13 +0300

    learn

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,10 @@
 
 import './App.css';
 
+// Формирование тегов
 function App() {
+
+  /*
   const str1 = 'text1';
 	const str2 = 'text1';
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -48,6 +51,106 @@ function App() {
         {addArr(arr)}
     </div>
   );
+  */
+
+/*
+   
+    const show = true;
+    const isAdult = true;
+    const isAdmin = false;
+    const isAuth = false;
+    function func(){
+      let text;
+      isAdmin ? text = <p className="admin">Admin</p> : text = <p className="noAdmin"> No Admin</p>;
+      return text;
+    }
+
+return(
+  
+    <div>
+      { show ? "text-true" : "text-false"}
+      <input type="text" className="input"/>
+      <p>{isAdult ? "Вам есть 18" : "Вам нет 18"}</p>
+        {func()}
+      {show && <p>text</p>}
+      {show || <p>text1</p>}
+      {isAuth && <p>Вы аворизованы</p>}
+      {isAuth || <p>Вы не аворизованы</p>}
+
+    </div>
+  );
+*/
+
+//Использование функций Реакт
+
+
+function square(num) {
+  return num ** 2;
+}
+
+function cube(num) {
+  return num ** 3;
+}
+
+const sum = <p>{square(3) + cube(4)}</p>;
+
+const getDigitsSum =(num)=>{
+    let result = 0;
+    for(let i = 0; i <= num; i++){
+      result += i;
+    }
+
+    return <p>{result}</p>
+}
+
+const showMess = (name)=> {
+  alert('hello' + name);
+}
+
+const show1 =()=> {
+  alert(1);
+}
+
+const show2 =()=> {
+  alert(2);
+}
+
+const showNum =(num)=>alert(`number ${num}`);
+
+function func(arg, event, arg1 ) {
+  console.log(arg);
+  console.log(arg1);
+  console.log(event); // объект с событием
+  console.log(event.target);
+  console.log(event.textContent);
+}
+
+const handleClick = (e) =>{
+  console.log("Значение this:", e)
+}
+
+return(
+  <div>
+    <input type="text"/>
+    {sum}
+    {getDigitsSum(12345)}
+
+    <button onClick={()=>showMess("user")}>show</button>
+    <button onClick={()=>showMess("User")}>show</button>
+    <button onClick={show1}>act1</button>
+		<button onClick={show2}>act2</button>
+    <button onClick={show2}>act2</button>
+    <button onClick={()=>showNum(1)}>act1</button>
+		<button onClick={()=>showNum(2)}>act2</button>
+    <button onClick={()=>showNum(3)}>act2</button>
+    <button onClick={event=>func("eee", event, "rrrr")}>Event</button>
+   <button onClick={(e)=>handleClick(e)}>Click</button>
+   
+  </div>
+ 
+);
+
+
 }
 
 export default App;
